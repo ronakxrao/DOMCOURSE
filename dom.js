9592656386a8b7271b487de2c6288d5ebb268cfd
ronakxrao <yadavronak301@gmail.com>
2024-07-03 10:34:41 +0530

    adding text and changing style

--- dom.js
+++ dom.js
@@ -27,3 +27,6 @@
 
 var items =document.getElementsByClassName('list-group-item');
 console.log(items);
+items[1].textContent = 'Hello 2';
+items[1].style.fontWeight = 'bold';
+items[1].style.backgroundcolor = 'yellow';
