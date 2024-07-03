104aa8646a010132adbb29ea8cdf31c102f4272f
ronakxrao <yadavronak301@gmail.com>
2024-07-03 10:26:36 +0530

    commented out everything

--- dom.js
+++ dom.js
@@ -1,24 +1,24 @@
 //EXAMINE THE DOCUMENT OBJECT
 // console.dir(document);
-console.log(document.domain);
-console.log(document.url);
-console.log(document.title);
+//console.log(document.domain);
+//console.log(document.url);
+//console.log(document.title);
 //document.title = 123;
-console.log(document.doctype);
-console.log(document.head);
-console.log(document.body);
-console.log(document.all);
-console.log(document.all[10]);
-console.log(document.forms);
-console.log(document.links);
+//console.log(document.doctype);
+//console.log(document.head);
+//console.log(document.body);
+//console.log(document.all);
+//console.log(document.all[10]);
+//console.log(document.forms);
+//console.log(document.links);
 
 //selectors
 // get element by id
 //console.log(document.getElementById('header-title'));
-var headerTitle =document.getElementById('header-title');
-var header = document.getElementById('main-header');
-console.log(headerTitle);
-headerTitle.textContent = 'Hello';
-headerTitle.innerHTML = '<h3>Hello</h3>';
+//var headerTitle =document.getElementById('header-title');
+//var header = document.getElementById('main-header');
+//console.log(headerTitle);
+//headerTitle.textContent = 'Hello';
+//headerTitle.innerHTML = '<h3>Hello</h3>';
 //headerTitle.style.borderBottom = 'solid 3px #000';
-header.style.borderBottom = 'solid 3px #000';
+//header.style.borderBottom = 'solid 3px #000';
