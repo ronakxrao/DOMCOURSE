f1202ea317e35c191d905cb171fc995d3c877d61
ronakxrao <yadavronak301@gmail.com>
2024-07-03 10:39:57 +0530

    changed background color using for loop

--- dom.js
+++ dom.js
@@ -30,3 +30,12 @@ console.log(items);
 items[1].textContent = 'Hello 2';
 items[1].style.fontWeight = 'bold';
 items[1].style.backgroundcolor = 'yellow';
+
+//to change colour of all
+//gives error
+
+//items.styke.backgroundcolor = 'f4f4f4';
+
+for(var i =0; i< items.length; i++){
+    items[i].style.backgroundColor = '#f4f4f4';
+}
