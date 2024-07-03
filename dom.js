0279c1615322d4a36afa2e5454f97e1143fcdc40
ronakxrao <yadavronak301@gmail.com>
2024-07-03 10:00:07 +0530

    Examine the document object

---
+++ dom.js
@@ -0,0 +1,11 @@
+//EXAMINE THE DOCUMENT OBJECT
+// console.dir(document);
+console.log(document.domain);
+console.log(document.url);
+console.log(document.title);
+//document.title = 123;
+console.log(document.doctype);
+console.log(document.head);
+console.log(document.body);
+console.log(document.all);
+//console.log(document.all[10]);
